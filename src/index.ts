991233dd081c5d194bda61d99c976c0e33eff384
Vish Patel <vishpatel913@googlemail.com>
2020-05-16 18:30:56 +0100

    set up prettier

--- src/index.ts
+++ src/index.ts
@@ -11,21 +11,24 @@ const router = new Router();
 const PORT = 3000;
 
 /** Middlewares */
-app.use( json() );
-app.use( logger() );
-app.use( bodyParser() );
+app.use(json());
+app.use(logger());
+app.use(bodyParser());
 
 /** Routes */
-app.use( router.routes() ).use( router.allowedMethods() );
+app.use(router.routes()).use(router.allowedMethods());
 
-router.get( "/", async (ctx: Koa.Context, next: () => Promise<any>) => {
-    ctx.body = { message: "This is your GET route" };
-    await next();
+router.get("/", async (ctx: Koa.Context, next: () => Promise<any>) => {
+  ctx.body = { message: "This is your GET route" };
+  await next();
 });
 
-router.post( "/data", async (ctx: Koa.Context, next: () => Promise<any>) => {
-    ctx.body = { message: "This is your POST route, attached you can find the data you sent", body: ctx.request.body };
-    await next();
+router.post("/data", async (ctx: Koa.Context, next: () => Promise<any>) => {
+  ctx.body = {
+    message: "This is your POST route, attached you can find the data you sent",
+    body: ctx.request.body,
+  };
+  await next();
 });
 
-app.listen( PORT, () => console.log( "Server started." ) );
+app.listen(PORT, () => console.log("Server started."));
