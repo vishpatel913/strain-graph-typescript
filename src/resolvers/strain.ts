93ea5ca384d871bbcd8f6b6d1085ee78f4a0c923
Vish Patel <vishpatel913@googlemail.com>
2020-11-04 17:47:23 +0000

    added singular strain placeholder

--- src/resolvers/strain.ts
+++ src/resolvers/strain.ts
@@ -1,5 +1,5 @@
-import { Resolver, Args, Query, Ctx, FieldResolver, Root } from "type-graphql";
-import { Strain, Effects } from "../entities";
+import { Resolver, Args, Query, Ctx, FieldResolver, Root, Arg } from "type-graphql";
+import { Strain, Effects, Race } from "../entities";
 import { SearchArgs } from "./types/search-args";
 import { DataSources, SearchKey, GetKey } from "../datasources/types";
 
@@ -16,6 +16,22 @@ export class StrainResolver {
     } else {
       return [];
     }
+  }
+
+  @Query(() => Strain, { nullable: false })
+  async strain(@Arg("delay", { defaultValue: 1 }) delay: number): Promise<Strain> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({
+          id: 1824,
+          name: "Stardawg",
+          desc:
+            "Stardawg, possibly named after the bright, sparkling crystal trichomes that blanket the strain like stars, is a hybrid cross between Chemdawg 4 and Tres Dawg. Earthy pine with sour notes of diesel color Stardawg, whose uplifting effects may help patients treating stress, fatigue, and anxiety disorders. Stardawg flowers in 67 days indoors with moderate yields. ",
+          race: Race.HYBRID,
+          effects: { positive: [], negative: [], medical: [] },
+        });
+      }, delay * 1000);
+    });
   }
 
   @FieldResolver(() => [String])
